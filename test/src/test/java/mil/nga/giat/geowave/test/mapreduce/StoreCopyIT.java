1a01c997d503ce58cbe55b4bffe478ebdc56b1ea
Kent Miller <kmiller@radiantblue.com>
2017-04-07 11:59:50 -0700

    fix copy/paste error

--- test/src/test/java/mil/nga/giat/geowave/test/mapreduce/StoreCopyIT.java
+++ test/src/test/java/mil/nga/giat/geowave/test/mapreduce/StoreCopyIT.java
@@ -104,7 +104,7 @@ public class StoreCopyIT
 				null);
 
 		command.setInputStoreOptions(
-				outputDataStorePluginOptions);
+				inputDataStorePluginOptions);
 		command.setOutputStoreOptions(
 				outputDataStorePluginOptions);
 

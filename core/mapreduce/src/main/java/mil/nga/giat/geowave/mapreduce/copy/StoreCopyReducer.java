a15d925b5d6b7cac2a03532058f8f875028e7454
Kent Miller <kmiller@radiantblue.com>
2017-04-10 11:33:55 -0700

    Added missing serializer

--- core/mapreduce/src/main/java/mil/nga/giat/geowave/mapreduce/copy/StoreCopyReducer.java
+++ core/mapreduce/src/main/java/mil/nga/giat/geowave/mapreduce/copy/StoreCopyReducer.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.ObjectWritable;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
 
 import mil.nga.giat.geowave.core.store.AdapterToIndexMapping;
 import mil.nga.giat.geowave.core.store.adapter.AdapterIndexMappingStore;
+import mil.nga.giat.geowave.mapreduce.HadoopWritableSerializationTool;
+import mil.nga.giat.geowave.mapreduce.HadoopWritableSerializer;
+import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputFormat;
 import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputKey;
 import mil.nga.giat.geowave.mapreduce.output.GeoWaveOutputFormat;
 import mil.nga.giat.geowave.mapreduce.output.GeoWaveOutputKey;
@@ -20,6 +27,7 @@ public class StoreCopyReducer extends
 		Reducer<GeoWaveInputKey, ObjectWritable, GeoWaveOutputKey, Object>
 {
 	private AdapterIndexMappingStore store;
+	private HadoopWritableSerializationTool serializationTool;
 
 	@Override
 	protected void setup(
@@ -28,6 +36,8 @@ public class StoreCopyReducer extends
 			InterruptedException {
 		super.setup(context);
 		store = GeoWaveOutputFormat.getJobContextAdapterIndexMappingStore(context);
+		serializationTool = new HadoopWritableSerializationTool(
+				GeoWaveInputFormat.getJobContextAdapterStore(context));
 	}
 
 	@Override
@@ -37,14 +47,29 @@ public class StoreCopyReducer extends
 			final Reducer<GeoWaveInputKey, ObjectWritable, GeoWaveOutputKey, Object>.Context context )
 			throws IOException,
 			InterruptedException {
-		final Iterator<ObjectWritable> objects = values.iterator();
+		final HadoopWritableSerializer<?, Writable> serializer = serializationTool
+				.getHadoopWritableSerializerForAdapter(key.getAdapterId());
+		
+		final Iterable<Object> transformedValues = Iterables.transform(
+				values,
+				new Function<ObjectWritable, Object>() {
+					@Override
+					public Object apply(
+							final ObjectWritable writable ) {
+						final Object innerObj = writable.get();
+						return (innerObj instanceof Writable) ? serializer.fromWritable((Writable) innerObj) : innerObj;
+					}
+				});
+
+
+		final Iterator<Object> objects = transformedValues.iterator();
 		while (objects.hasNext()) {
 			final AdapterToIndexMapping mapping = store.getIndicesForAdapter(key.getAdapterId());
 			context.write(
 					new GeoWaveOutputKey<>(
 							mapping.getAdapterId(),
 							Arrays.asList(mapping.getIndexIds())),
-					objects.next().get());
+					objects.next());
 		}
 	}
 

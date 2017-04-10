a15654a601e69f498dea395fad22eda813a7a313
Kent Miller <kmiller@radiantblue.com>
2017-04-10 12:08:21 -0700

    Refactor reducer to use base class

--- core/mapreduce/src/main/java/mil/nga/giat/geowave/mapreduce/copy/StoreCopyReducer.java
+++ core/mapreduce/src/main/java/mil/nga/giat/geowave/mapreduce/copy/StoreCopyReducer.java
@@ -5,17 +5,11 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.ObjectWritable;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Iterables;
 
 import mil.nga.giat.geowave.core.store.AdapterToIndexMapping;
 import mil.nga.giat.geowave.core.store.adapter.AdapterIndexMappingStore;
-import mil.nga.giat.geowave.mapreduce.HadoopWritableSerializationTool;
-import mil.nga.giat.geowave.mapreduce.HadoopWritableSerializer;
-import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputFormat;
+import mil.nga.giat.geowave.mapreduce.GeoWaveWritableInputReducer;
 import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputKey;
 import mil.nga.giat.geowave.mapreduce.output.GeoWaveOutputFormat;
 import mil.nga.giat.geowave.mapreduce.output.GeoWaveOutputKey;
@@ -24,10 +18,9 @@ import mil.nga.giat.geowave.mapreduce.output.GeoWaveOutputKey;
  * A basic implementation of copy as a reducer
  */
 public class StoreCopyReducer extends
-		Reducer<GeoWaveInputKey, ObjectWritable, GeoWaveOutputKey, Object>
+GeoWaveWritableInputReducer<GeoWaveOutputKey, Object>
 {
 	private AdapterIndexMappingStore store;
-	private HadoopWritableSerializationTool serializationTool;
 
 	@Override
 	protected void setup(
@@ -36,33 +29,16 @@ public class StoreCopyReducer extends
 			InterruptedException {
 		super.setup(context);
 		store = GeoWaveOutputFormat.getJobContextAdapterIndexMappingStore(context);
-		serializationTool = new HadoopWritableSerializationTool(
-				GeoWaveInputFormat.getJobContextAdapterStore(context));
 	}
 
 	@Override
-	protected void reduce(
-			final GeoWaveInputKey key,
-			final Iterable<ObjectWritable> values,
-			final Reducer<GeoWaveInputKey, ObjectWritable, GeoWaveOutputKey, Object>.Context context )
+	protected void reduceNativeValues(
+			GeoWaveInputKey key,
+			Iterable<Object> values,
+			Reducer<GeoWaveInputKey, ObjectWritable, GeoWaveOutputKey, Object>.Context context )
 			throws IOException,
 			InterruptedException {
-		final HadoopWritableSerializer<?, Writable> serializer = serializationTool
-				.getHadoopWritableSerializerForAdapter(key.getAdapterId());
-		
-		final Iterable<Object> transformedValues = Iterables.transform(
-				values,
-				new Function<ObjectWritable, Object>() {
-					@Override
-					public Object apply(
-							final ObjectWritable writable ) {
-						final Object innerObj = writable.get();
-						return (innerObj instanceof Writable) ? serializer.fromWritable((Writable) innerObj) : innerObj;
-					}
-				});
-
-
-		final Iterator<Object> objects = transformedValues.iterator();
+		final Iterator<Object> objects = values.iterator();
 		while (objects.hasNext()) {
 			final AdapterToIndexMapping mapping = store.getIndicesForAdapter(key.getAdapterId());
 			context.write(
